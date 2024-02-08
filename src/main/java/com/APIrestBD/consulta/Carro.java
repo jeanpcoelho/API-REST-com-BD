6cdab32c0856da56ca704d3b213643372254e9fb
jeanpcoelho <jeanpc0018@gmail.com>
2024-02-08 13:29:11 -0300

    Update Carro.java
    simplificação e melhoria do código

--- src/main/java/com/APIrestBD/consulta/Carro.java
+++ src/main/java/com/APIrestBD/consulta/Carro.java
@@ -24,6 +24,8 @@ public class Carro {
 
     public Carro() {}
 
+    // Getters e Setters
+
     public Long getId() {
         return id;
     }
@@ -56,34 +58,28 @@ public class Carro {
         this.ano = ano;
     }
 
+    // hashCode, equals e toString
+
     @Override
     public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
+        return Objects.hash(id);
     }
 
     @Override
     public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
         Carro other = (Carro) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
+        return Objects.equals(id, other.id);
     }
 
     @Override
     public String toString() {
-        return "Carro [id=" + id + ", nome=" + nome + "]";
+        return "Carro{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", modelo='" + modelo + '\'' +
+                ", ano=" + ano +
+                '}';
     }
-
 }
